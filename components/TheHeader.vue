1acd2cedb265e1b809f27397212a5abbcea1186e
FrediNans <80039008+FrediNans@users.noreply.github.com>
2021-10-20 10:18:01 +0200

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+	<header>
+		<h1>logo</h1>
+		<NuxtLink to="/">Home page</NuxtLink> |
+		<NuxtLink to="/admin">Admin page</NuxtLink>
+	</header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>test</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	head: {
+		title: "home",
+		meta: [
+			{
+				name: "description",
+				content:
+					"If you would prefer to use the options syntax, you can do exactly the same thing within your component options with an object or function called head.",
+			},
+		],
+	},
+};
+</script>
